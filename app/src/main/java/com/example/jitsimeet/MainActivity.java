b78ccbcd4e44262970eedd581db57570105a1d4d
priyanshu <priyanshukumar025@gmail.com>
2021-06-17 01:04:51 +0530

    changing layout and hiding keyboard

--- app/src/main/java/com/example/jitsimeet/MainActivity.java
+++ app/src/main/java/com/example/jitsimeet/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.jitsimeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import org.jitsi.meet.sdk.JitsiMeet;
@@ -14,11 +16,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       editText=findViewById(R.id.editText);
+        InputMethodManager mgr=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(editText.getWindowToken(),0);
 
         try {
             JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
@@ -30,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void OnButtonClick(View view){
-        EditText editText=findViewById(R.id.editText);
+
         String text=editText.getText().toString();
         if(text.length()>0){
             JitsiMeetConferenceOptions options=new JitsiMeetConferenceOptions.Builder()
--- app/src/main/java/com/example/jitsimeet/splashScreen.java
+++ app/src/main/java/com/example/jitsimeet/splashScreen.java
@@ -13,7 +13,7 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         new Handler().postDelayed(new Runnable() {
             @Override
